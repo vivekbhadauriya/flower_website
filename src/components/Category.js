1d8509134a0351f3de8b6dbf78e8d712e090de34
vivekbhadauriya <vkthakur6.7.9.8@gmail.com>
2024-10-02 00:12:21 +0530

    Updated

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import "./AppCss.css"
+import { useNavigate } from 'react-router-dom';
+
 
 const Category = ({ title, image }) => {
+    const navigate = useNavigate();
+
+    const handleShopNowClick = () => {
+        navigate('/productlist');  // Navigates to the product page
+    };
     return (
         <div className="category">
             <div className="category-image" style={{ backgroundImage: `url(${image})` }}></div>
             <div className="category-content">
                 <h3>{title}</h3>
-                <button className="shop-now">SHOP NOW</button>
+                <button className="shop-now" onClick={handleShopNowClick} style={{ cursor: 'pointer' }}>SHOP NOW</button>
             </div>
         </div>
     )
